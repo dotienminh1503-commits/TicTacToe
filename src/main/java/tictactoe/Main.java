a160f7f78b2a80a9373e66d72f891a098deb6970
DoMinh <dotienminh1503@gmail.com>
2026-03-25 00:47:04 +0700

    while nhap toa do

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -8,6 +8,33 @@ public class Main {
                 {'-', '-', '-'},
                 {'-', '-', '-'}
         };
+        Scanner scanner = new Scanner(System.in);
+        char p1 = 'x';
+        boolean gameEnded =  false;
+
+        while (!gameEnded) {
+            drawBoard(board);
+            System.out.println("Luot cua nguoi choi" + p1 );
+
+            int row = -1;
+            int col = -1;
+
+            while (true) {
+                System.out.println("Nhap vi tri hang 1 -> 3: ");
+                row = scanner.nextInt() -1;
+                System.out.println("Nhap vi tri cot 1 -> 3: ");
+                col = scanner.nextInt() -1;
+                if (row<0 || row>0 || col < 0 || col > 0) {
+                    System.out.println({"Vi tri khong hop le");
+                }
+                else if (board[row][col] != '-') {
+                    System.out.println("Chon o khac ");
+                }
+                else {
+                    break;
+                }
+            }
+        }
 
     }
 }
